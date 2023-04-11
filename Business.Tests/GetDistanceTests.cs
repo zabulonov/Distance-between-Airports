1a6b575c2ac41e037603c662e68f19610346a5f5
Alexey Zabulonov <zabulonov444@yandex.ru>
2023-04-11 08:08:15 +0300

    edit tests

--- Business.Tests/GetDistanceTests.cs
+++ Business.Tests/GetDistanceTests.cs
@@ -35,13 +35,10 @@ public class GetDistanceTests
         // Я не понимаю как это работает, мне страшно трогать
         var cacheMock = new Mock<IDistributedCache>();
         cacheMock.Setup(c => c.GetAsync(firstCode, default)).ReturnsAsync((byte[])null);
-        cacheMock.Setup(cc => cc.GetAsync(secondCode, default)).ReturnsAsync((byte[])null);
-        byte[] newData = new byte[] { 4, 5, 6 };
+        cacheMock.Setup(c => c.GetAsync(secondCode, default)).ReturnsAsync((byte[])null);
         var options = new DistributedCacheEntryOptions();
         options.SetSlidingExpiration(TimeSpan.FromMinutes(5));
-        cacheMock.Setup(c => c.SetAsync("mykey", newData, options, default)).Returns(Task.CompletedTask);
-
-
+        cacheMock.Setup(c => c.SetAsync("mykey", null, options, default)).Returns(Task.CompletedTask);
         var mock = new MockAirportInfoServiceProxy(_tests);
 
         var airportDistanceService = new AirportDistanceService(mock, cacheMock.Object);
